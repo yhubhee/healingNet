fe373e1edb8a348b0ead78719163b1b2c0c862aa
yhubhee <emenyiubongabasi415@gmail.com>
2025-06-12 04:02:43 +0100

    updated commit

--- database.js
+++ database.js
@@ -7,10 +7,10 @@ const pool = mysql.createPool({
     password: process.env.DATABASE_PASS,
     database: process.env.DATABASE,
     port: process.env.PORT, 
-    connectionLimit: 90, // Increase the connection limit if necessary
-    connectTimeout: 90000,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 10000
+    // connectionLimit: 790, // Increase the connection limit if necessary
+    // connectTimeout: 90000,
+    // enableKeepAlive: true,
+    // keepAliveInitialDelay: 10000
 });
 
 pool.getConnection((err, connection) => {
